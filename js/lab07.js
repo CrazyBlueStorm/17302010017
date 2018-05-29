8ee16c104f841ee296a8c33e7dca7898eab16cae
CrazyBlueStorm <3142037382@qq.com>
2018-05-29 23:21:05 +0800

    lab09

--- js/lab07.js
+++ js/lab07.js
@@ -1,197 +1,70 @@
 let button = document.getElementsByTagName("button")[0];
 
-let colNumber = document.getElementById("colNumber");
+let columnNumber = document.getElementById("columnsNumbers");
+
+let select1 = document.getElementById("select1");
+
+let select2 = document.getElementById("select2");
 
 let aboutColumn = document.querySelector(".aboutColumn");
 
-let tbCreate = document.querySelector(".tbCreate");
+let tableCreate = document.querySelector(".tableCreate");
 
 let layTable = document.querySelector(".lay-table");
 
 let information = document.querySelector(".information");
 
-let select2 = document.getElementById("select2");
+let aboutTable = document.querySelector(".aboutTable");
 
-let select1 = document.getElementById("select1");
+let columnsInput = [];
 
-let aboutTb = document.querySelector(".aboutTb");
+let tables = [];
 
-let columnsInput = [];//储存输入框信息
+let classes = ["","pink"];
 
-let tables = [];//储存表格
-
-let classes = ["", "grey"];
-
-//列数改变，输入框个数改变
-colNumber.onchange = function(){
-    let number = parseInt(colNumber.value);
-
-    if(number <= 0){
+function showTable(table){
+    if(layTable.firstChild)
+        layTable.removeChild(layTable.firstChild);
+    if(select2.value === "select")
         return;
-    }
-    else {
-        button.style.display = "inline";
-        aboutColumn.style.display = "block";
-        showInput(number);
-    }
-};
+    layTable.appendChild(table);
+}
 
-//显示一定数量的输入框
-function showInput(number, ths = []){
+function showInput(number,ths = []){
     while(aboutColumn.hasChildNodes()){
         aboutColumn.removeChild(aboutColumn.firstChild);
     }
-
     for(let i = 0; i < number; i++){
         columnsInput[i] = document.createElement("input");
         columnsInput[i].type = "text";
-        columnsInput[i].placeholder = ths[i]? ths[i].innerHTML : "Attribute";
+        columnsInput[i].placeholder = this[i] ? ths[i].innerHTML : "Attribute";
         columnsInput[i].style.width = 140 + "px";
         columnsInput[i].style.height = "30px";
         aboutColumn.appendChild(columnsInput[i]);
     }
 }
 
-select1.onchange = function(){
-    switch (this.value) {
-        case "selectOne":
-            button.style.display = "none";
-            tbCreate.style.display = "none";
-            information.innerHTML = "";
-        break;
-
-        case "createTable":
-            aboutTb.getElementsByTagName("input")[0].value = "";
-            aboutTb.getElementsByTagName("input")[1].value = "";
-            tbCreate.style.display = "block";
-            aboutTb.style.display = "block";
-            aboutColumn.style.display = "none";
-            information.innerHTML = "";
-        break;
-
-        case "addRow":
-            aboutTb.style.display = "none";
-            aboutColumn.style.display = "block";
-            addRow();
-            information.innerHTML = "";
-        break;
-
-        case "delRow":
-            aboutTb.style.display = "none";
-            aboutColumn.style.display = "block";
-            if(select2.value != "select")
-                showInput(tables[select2.value].getElementsByTagName("th").length,tables[select2.value].getElementsByTagName("th"));
-            information.innerHTML = "";
-        break;
-
-        case "delTable":
-            tbCreate.style.display = "none";
-            information.innerHTML = "WARNING: You cannot undo this action!";
-        break;
-    }
-};
-
-select2.onchange = function(){
-    showTable(tables[select2.value]);
-    if(select2.value == "select")
-        return;
-    if(select1.value == "addRow" || select1.value == "delRow"){
-        showInput(tables[select2.value].getElementsByTagName("th").length, numbers = tables[select2.value].getElementsByTagName("th"));
-    }
-};
-
-//添加一行所显示的input
 function addRow(){
     let numbers = 0;
-
-    if(select2.value != "select")
+    if(select2.value !== "select")
         numbers = tables[select2.value].getElementsByTagName("th").length;
-    else
+    else{
         return;
-    showInput(numbers,numbers = tables[select2.value].getElementsByTagName("th"));
+    }
+    showInput(numbers, numbers = tables[select2.value].getElementsByTagName("th"));
 }
-
-//按钮提交事件
-button.onclick = function(){
-    if(select1.value == "createTable"){
-        let tbName = document.getElementById("tbName").value || "tableName";
-        addOption(tbName);
-        tbName = handleTaName(tbName);
-        tables[tbName] = document.createElement("table");
-        let thead = document.createElement("thead");
-        for(let i = 0; i < parseInt(colNumber.value); i++){
-            let th = document.createElement("th");
-            th.innerHTML = columnsInput[i].value || "Attribute";
-            thead.appendChild(th);
-        }
-        tables[tbName].appendChild(thead);
-        showTable(tables[tbName]);
-        aboutTb.getElementsByTagName("input")[0].value = "";
-        aboutTb.getElementsByTagName("input")[1].value = "";
-        showInput(0);
-    }
-    else if(select1.value == "addRow"){
-        let ok = false;
-        let tr = document.createElement("tr");
-        for(let i = 0; i < tables[select2.value].getElementsByTagName("th").length; i++){
-            let td = document.createElement("td");
-            td.innerHTML = columnsInput[i].value;
-            td.className = classes[tables[select2.value].getElementsByTagName("tr").length % 2];
-            tr.appendChild(td);
-            if(td.innerHTML !== "")
-                ok = true;
-        }
-        if(!ok){
-            return false;
-        }
-        tables[select2.value].appendChild(tr);
-        showInput(tables[select2.value].getElementsByTagName("th").length,tables[select2.value].getElementsByTagName("th"));
-    }
-    else if(select1.value == "delTable"){
-        delOption();
-        information.innerHTML = "";
-    }
-    else if(select1.value == "delRow"){
-        if(select2.value == "select")
-            return false;
-        let trs = tables[select2.value].getElementsByTagName("tr");
-        let ok2 = true;
-        for(let i = 0; i < trs.length; i++){
-            let ok = true;//匹配判断常量
-            let tds = trs[i].getElementsByTagName("td");
-            for(let k = 0; k < tds.length; k++){
-                if(!ok2){
-                    tds[k].className = classes[1 - classes.indexOf(tds[k].className)]
-                }
-                if(tds[k].innerHTML !== aboutColumn.getElementsByTagName("input")[k].value
-                    && aboutColumn.getElementsByTagName("input")[k].value && ok2){
-                    ok = false;
-                    break;
-                }
-            }
-            if(ok && ok2){
-                tables[select2.value].removeChild(trs[i]);
-                showInput(tables[select2.value].getElementsByTagName("th").length,tables[select2.value].getElementsByTagName("th"));
-                ok2 = false;
-                i--;
-            }
-        }
-        showInput(tables[select2.value].getElementsByTagName("th").length,tables[select2.value].getElementsByTagName("th"));
-    }
-    return false;
-};
 
 function addOption(optionValue){
     let option = document.createElement("option");
     select2.appendChild(option);
     option.innerHTML = optionValue;
-    option.value = handleTaName(optionValue);
+    option.value = handleTableName(optionValue);
     option.selected = true;
 }
 
-function delOption(){
+function deleteOption(){
     let options = select2.getElementsByTagName("option");
-    if(select2.value == "select")
+    if(select2.value === "select")
         return;
     for(let option of options){
         if(option.selected){
@@ -199,7 +72,7 @@ function delOption(){
             tables[option.value] = "";
             if(layTable.firstChild)
                 layTable.removeChild(layTable.firstChild);
-            if(select2.getElementsByTagName("option")[1]){
+            if(select2.getElementsByTagName(option)[1]){
                 select2.getElementsByTagName("option")[1].selected = true;
                 showTable(tables[select2.getElementsByTagName("option")[1].value]);
             }
@@ -208,19 +81,135 @@ function delOption(){
     }
 }
 
-function handleTaName(tableName){
-    let count = 0;//计数器
+function handleTableName(tableName){
+    let count = -0;
     for(let i in tables){
-        if(i.replace( /\d+/,'') == tableName.replace( /\d+/,''))
+        if (i.replace(/\d+/,'') === tableName.replace(/\d+/,''))
             count++;
     }
     return tableName + count;
 }
 
-function showTable(table){
-    if(layTable.firstChild)
-        layTable.removeChild(layTable.firstChild);
-    if(select2.value == "select")
+select1.onchange = function(){
+    switch(this.value){
+        case "selectOne":
+            button.style.display = "none";
+            tableCreate.style.display = "none";
+            information.innerHTML = "";
+            break;
+
+        case "createTable":
+            aboutTable.getElementsByTagName("input")[0].value = "";
+            aboutTable.getElementsByTagName("input")[1].value = "";
+            tableCreate.style.display = "block";
+            aboutTable.style.display = "block";
+            aboutColumn.style.display = "none";
+            information.innerHTML = "";
+            break;
+
+        case "addRow":
+            aboutTable.style.display = "none";
+            aboutColumn.style.diaplay = "block";
+            addRow();
+            information.innerHTML = "";
+            break;
+
+        case "deleteRow":
+            aboutTable.style.diaplay = "none";
+            aboutColumn.style.diaplay = "block";
+            if(select2.value !== "select")
+                showInput(tables[select2.value].getElementsByTagName("th").length, tables[select2.value].getElementsByTagName("th"));
+            information.innerHTNL = "";
+            break;
+
+        case "deleteTable":
+            tableCreate.style.display = "none";
+            information.innerHTML = "Attention: This is an irreversible action";
+            if (select2.value === "select")
+                information.innerHTML = "";
+            break;
+    }
+};
+
+select2.onchange = function(){
+    showTable(tables[select2.value]);
+    if (select2.value === "select")
         return;
-    layTable.appendChild(table);
-}
+    if (select1.value ==="addRow" || select1.value ==="deleteRow"){
+        showInput(tables[select2.value].getElementsByTagName("th").length, numbers = tables[select2.value].getElementsByTagName("th"));
+    }
+};
+
+columnNumber.onchange = function(){
+    let number = parseInt(columnNumber.value);
+    if (number <= 0)
+        return false;
+    else {
+        button.style.display = "inline";
+        aboutColumn.style.display = "block";
+        showInput(number);
+    }
+};
+
+button.onclick = function() {
+    if (select1.value === "createTable") {
+        let tableName = document.getElementById("tableName").value || "tableName";
+        addOption(tableName);
+        tableName = handleTableName(tableName);
+        tables[tableName] = document.createElement("table");
+        let thead = document.createElement("tr");
+        for (let i = 0; i < parseInt(columnNumber.value); i++) {
+            let th = document.createElement("th");
+            th.innerHTML = columnsInput[i].value || "Attribute";
+            thead.appendChild(th);
+        }
+        tables[tableName].appendChild(thead);
+        showTable(tables[tableName]);
+        aboutTable.getElementsByTagName("input")[0].value = "";
+        aboutTable.getElementsByTagName("input")[1].value = "";
+        showInput(0);
+    } else if (select1.value === "addRow") {
+        let flag = false;
+        let tr = document.createElement("tr");
+        for (let i = 0; i < tables[select2.value].getElementsByTagName("th").length; i++) {
+            let td = document.createElement("td");
+            td.innerHTML = columnsInput[i].value;
+            td.className = classes[tables[select2.value].getElementsByTagName("tr").length % 2];
+            tr.appendChild(td);
+            if (td.innerHTML !== "")
+                flag = true;
+        }
+        if (!flag)
+            return false;
+        tables[select2.value].appendChild(tr);
+        showInput(tables[select2.value].getElementsByTagName("th").length, tables[select2.value].getElementsByTagName("th"));
+    } else if (select1.value === "deleteTable") {
+        deleteOption();
+        if (select2.value === "select")
+            information.innerHTML = "";
+    } else if (select1.value === "deleteRow") {
+        if (select2.value === "select")
+            return false;
+        let flag2 = true;
+        for (let i = 0; i < trs.length; i++) {
+            let flag = true;
+            let tds = trs[i].getElementsByTagName("td");
+            for (let j = 0; j < tds; j++) {
+                if (!flag2)
+                    tds[j].className = classes[1 - classes.indexOf(tds[j].className)];
+                if (tds[j].innerHTML !== aboutColumn.getElementsByTagName("input")[j].value && aboutColumn.getElementsByTagName("input")[j].value && flag2) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag && flag2) {
+                tables[select2.value].removeChild(trs[i]);
+                showInput(tables[select2.value].getElementsByTagName("th").length, tables[select2.value].getElementsByTagName("th"));
+                flag2 = false;
+                i--;
+            }
+        }
+        showInput(tables[select2.value].getElementsByTagName("th").length, tables[select2.value].getElementsByTagName("th"));
+    }
+    return false;
+};
